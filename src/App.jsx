ade98a05a767bbd169109ad6899f243fb5fe8a9b
senyomo <lumusenyomoandrew@gmail.comm>
2024-06-27 13:22:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  
+  
+
+  return (<>
+  
+  <div className="h-5/6"></div>
+  </>)
+}
+
+
+  
---
+++ src/es6-classes-1.js
@@ -0,0 +1,9 @@
+class Person{
+
+
+}
+
+const me = new Person();
+console.log(me)
+
+export default cs6-classes-1
